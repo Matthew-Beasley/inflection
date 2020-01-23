f3a929ba9ca136691b1ebe238ce5baa84160a611
conbec <56614504+conbec@users.noreply.github.com>
2020-01-22 17:55:58 -0800

    working, needs styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import inflection, {plural, singular} from 'inflection';
 import './App.css';
 
 function App() {
+  const [wordCards, setWordCards] = useState([])
+  const [word, setWord] = useState('');
+
+  const getCards = () => {
+    const newCard = {
+      key: wordCards.length,
+      singular: inflection.pluralize(word, plural) ,
+      plural: inflection.singularize(word, singular)
+    }
+    setWordCards([...wordCards, newCard])
+  }
+
+  const deleteCards = ({target}) => {
+    const updated = wordCards.filter(card => {
+      if (!target.classList.contains(card.key)) {
+        return card;
+      }
+    })
+    console.log(updated)
+    setWordCards([...updated])
+    //console.log(wordCards)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-section">
+        <input type="text" value={word} onChange={(ev) => setWord(ev.target.value)}></input>
+        <button onClick={(ev) => getCards(ev)}>Get Word</button>
+      </div>
+      <div className="output-section">
+        {wordCards.map(card => {
+          return (
+            <div className="card" key={card.key}>
+              <p>{card.singular}</p>
+              <p>{card.plural}</p>
+              <button className={card.key} onClick={(ev) => deleteCards(ev)}>X</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
